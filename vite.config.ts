d839046c55c146a7668fbd57e9d8a80e9b9375f4
Danilo Ramirez <danilram@cisco.com>
2025-07-16 08:04:14 -0600

    Adding build time options

--- vite.config.ts
+++ vite.config.ts
@@ -5,8 +5,10 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   base:'/ZenIcons/',
+  publicDir:'public',
   build:{
     outDir: './docs',
-    emptyOutDir: true
+    emptyOutDir: true,
+    reportCompressedSize: true,
   }
 })
